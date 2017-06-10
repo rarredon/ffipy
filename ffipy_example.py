f3f52ec0f5bf00b3fd9d77230d3802b155aa0c9b
Ryan Arredondo <sucharyan@gmail.com>
2017-06-10 14:11:55 -0600

    Pep8'ed ffipy_example

--- ffipy_example.py
+++ ffipy_example.py
@@ -8,11 +8,12 @@
 
 from ffipy import FFIEC_Client
 
+
 def main():
     client = FFIEC_Client()
-    
+
     # Initialize variables for data pull
-    ds_name='Call'    # Pull Call report data
+    ds_name = 'Call'    # Pull Call report data
     end_date = '3/31/2017'  # Pull data in reporting pd ending 3/31/17
     fiID_type = 'ID_RSSD'  # Type of financial inst identifier
     fiID = 64150    # Indentifier for Wyomin Bank and Trust
@@ -22,7 +23,7 @@ def main():
 
     # Get report
     client.retrieve_facsimile(ds_name=ds_name, reporting_pd_end=end_date,
-                              fiID_type=fiID_type, fiID = fiID,
+                              fiID_type=fiID_type, fiID=fiID,
                               facsimile_fmt=fmt, outfile=outfile,
                               return_result=False)
 
